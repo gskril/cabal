abf635c1f86af2607c30ef2cca379fb041b606b7
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-11-23 23:36:05 -0500

    Clean up

--- apps/contracts/src/Cabal.sol
+++ apps/contracts/src/Cabal.sol
@@ -2,9 +2,6 @@
 pragma solidity 0.8.23;
 
 import {ISemaphore} from "@semaphore-protocol/contracts/interfaces/ISemaphore.sol";
-import {IERC1155} from "@openzeppelin/contracts/interfaces/IERC1155.sol";
-import {SignatureChecker} from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
-import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 ///////////////////////////////////////////////////////////////////////
 //                                                                   //
@@ -18,25 +15,12 @@ import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/Messa
 //                                                                   //
 ///////////////////////////////////////////////////////////////////////
 
-/// @notice A shared smart contract account that can be used by multiple users.
-/// @dev This is effectively a 1/n multisig wallet that can call arbitrary functions on any smart contract, sign messages, etc.
+/// @notice A shared, anonymous, invite-only smart contract account.
+/// @dev Effectively a 1/n multi-sig wallet that can call arbitrary functions, sign ERC-1271 messages, etc.
 contract Cabal {
     /*//////////////////////////////////////////////////////////////
                                 STRUCTS
     //////////////////////////////////////////////////////////////*/
-
-    /// @notice A struct that contains the intent of a member to execute a call.
-    /// @custom:field messageHash The hash of the calldata that the member wants to execute.
-    /// @custom:field signature The signature of the member.
-    /// @custom:field pubKey The public key of the member.
-    struct SemaphoreIntent {
-        address to;
-        uint256 value;
-        bytes data;
-        bytes32 messageHash;
-        bytes32 signature;
-        bytes pubKey;
-    }
 
     /*//////////////////////////////////////////////////////////////
                                PARAMETERS
@@ -52,15 +36,22 @@ contract Cabal {
     uint256 public immutable semaphoreGroupId;
 
     /// @notice The token that relayer fees are paid in.
+    /// @dev address(0) for ETH, otherwise an ERC-20 token address. Defaults to ETH.
+    address public feeToken;
 
-    /// @notice The amount of {token} that is paid to a relayer for executing a transaction.
+    /// @notice The amount of {feeToken} that is paid to a relayer for executing a transaction.
+    /// @dev Defaults to 0.
+    uint256 public feeAmount;
 
     /*//////////////////////////////////////////////////////////////
                                  EVENTS
     //////////////////////////////////////////////////////////////*/
 
     event Received(address indexed sender, uint256 indexed value);
-    event ExecutionSuccess(bytes32 indexed txHash, uint256 indexed payment);
+    event MemberAdded(uint256 indexed identityCommitment);
+    event MemberRemoved(uint256 indexed identityCommitment);
+    event ExecutionSuccess(uint256 indexed value);
+    event ExecutionFailure();
 
     /*//////////////////////////////////////////////////////////////
                                  ERRORS
@@ -69,17 +60,6 @@ contract Cabal {
     error ExecutionFailed();
     error InvalidProof();
     error InvalidIntent();
-    /*//////////////////////////////////////////////////////////////
-                               MODIFIERS
-    //////////////////////////////////////////////////////////////*/
-
-    // TODO: we need a way to stuff the proof with intended calldata or something so that it can only be used as intended
-    // https://docs.semaphore.pse.dev/guides/identities#sign-and-verify-messages
-    modifier onlyMember(ISemaphore.SemaphoreProof calldata proof, SemaphoreIntent calldata intent) {
-        semaphore.validateProof(semaphoreGroupId, proof);
-        if (intent.messageHash != keccak256(abi.encode(intent.to, intent.value, intent.data))) revert InvalidIntent();
-        _;
-    }
 
     /*//////////////////////////////////////////////////////////////
                               CONSTRUCTOR
@@ -100,30 +80,39 @@ contract Cabal {
         emit Received(msg.sender, msg.value);
     }
 
-    /// @notice Executes a call to an arbitrary contract.
-    /// @dev This function is protected by the Semaphore proof, but can be called by anyone. This is meant to be used with relayers to preserve privacy.
-    function execute(
-        address to,
-        bytes calldata data,
-        uint256 value,
-        ISemaphore.SemaphoreProof calldata proof,
-        SemaphoreIntent calldata intent
-    ) public {
-        // Verify the intent matches the calldata
-        if (intent.messageHash != keccak256(abi.encode(to, value, data))) revert InvalidIntent();
-
-        // Validate the Semaphore prof
-        semaphore.validateProof(semaphoreGroupId, proof);
-
-        (bool success,) = to.call{value: value}(data);
-        if (!success) revert ExecutionFailed();
+    /// @dev This is the equivalent of calling `semaphore.addMember(semaphoreGroupId, identityCommitment);`
+    /// @dev Uses `execute()` for consistency, even though a custom `proof.message` would be simpler.
+    function addMember(uint256 identityCommitment, ISemaphore.SemaphoreProof calldata proof) external {
+        bytes4 selector = semaphore.addMember.selector;
+        bytes memory data = abi.encodeWithSelector(selector, semaphoreGroupId, identityCommitment);
+        execute(address(semaphore), 0, data, proof);
     }
 
-    function addMember(uint256 identityCommitment, ISemaphore.SemaphoreProof calldata proof) public onlyMember(proof) {
-        semaphore.addMember(semaphoreGroupId, identityCommitment);
+    /// @notice Executes a call to an arbitrary contract.
+    /// @dev Protected by a Semaphore proof, but can be called by anyone (e.g. relayers to preserve privacy).
+    function execute(address to, uint256 value, bytes memory data, ISemaphore.SemaphoreProof calldata proof)
+        public
+        virtual
+    {
+        // Check if the Semaphore proof is valid and hasn't already been used
+        semaphore.validateProof(semaphoreGroupId, proof);
+
+        // Check if the calldata matches `proof.message` so relayers can't execute arbitrary calls
+        bytes32 intentHash = keccak256(abi.encode(to, value, data));
+        if (proof.message != uint256(intentHash)) revert InvalidIntent();
+
+        // Execute the intended call
+        (bool success,) = to.call{value: value}(data);
+        if (success) emit ExecutionSuccess(value);
+        else emit ExecutionFailure();
     }
 
     /*//////////////////////////////////////////////////////////////
-                           INTERNAL FUNCTIONS
+                            HELPER FUNCTIONS
     //////////////////////////////////////////////////////////////*/
+
+    /// @notice Checks if a Semaphore proof is valid.
+    function isValidProof(ISemaphore.SemaphoreProof calldata proof) external view returns (bool) {
+        return semaphore.verifyProof(semaphoreGroupId, proof);
+    }
 }
