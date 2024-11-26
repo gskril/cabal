64a4b7741dd8a7c862e539850fc64ebce3de862d
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-11-26 02:23:51 -0500

    Setup hono + zod

--- apps/relayer/src/index.ts
+++ apps/relayer/src/index.ts
@@ -1,1 +1,9 @@
-console.log('hi')
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+  .get('/', (c) => c.text('Hello Node.js!'))
+  .post('/', (c) => c.text('From Post'))
+
+serve({ fetch: app.fetch, port: 3000 })
+console.log('Server is running on http://localhost:3000')
