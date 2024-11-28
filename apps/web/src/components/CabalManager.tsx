640a197ba16700a73c00f4f1ddf10d9d3aba695f
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-11-28 11:50:46 -0500

    Move transaction builder to its own component

---
+++ apps/web/src/components/CabalManager.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+export function CabalManager() {
+  return (
+    <div>
+      <p>
+        Cabal is an anonymous, shared smart contract wallet powered by{' '}
+        <a
+          href="https://semaphore.pse.dev/"
+          target="_blank"
+          className="underline"
+        >
+          Semaphore
+        </a>
+        .
+      </p>
+    </div>
+  )
+}
