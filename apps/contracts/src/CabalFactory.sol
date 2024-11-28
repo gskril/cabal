483fe8c40b26f6f2171e8e21660e31d60e1f3ddf
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-11-28 10:43:52 -0500

    Add ASCII art to CabalFactory

--- apps/contracts/src/CabalFactory.sol
+++ apps/contracts/src/CabalFactory.sol
@@ -4,6 +4,18 @@ pragma solidity 0.8.23;
 import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
 import {Cabal} from "./Cabal.sol";
 
+///////////////////////////////////////////////////////////////////////
+//                                                                   //
+//      .g8"""bgd     db      `7MM"""Yp,      db      `7MMF'         //
+//    .dP'     `M    ;MM:       MM    Yb     ;MM:       MM           //
+//    dM'       `   ,V^MM.      MM    dP    ,V^MM.      MM           //
+//    MM           ,M  `MM      MM"""bg.   ,M  `MM      MM           //
+//    MM.          AbmmmqMA     MM    `Y   AbmmmqMA     MM      ,    //
+//    `Mb.     ,' A'     VML    MM    ,9  A'     VML    MM     ,M    //
+//      `"bmmmd'.AMA.   .AMMA..JMMmmmd9 .AMA.   .AMMA..JMMmmmmMMM    //
+//                                                                   //
+///////////////////////////////////////////////////////////////////////
+
 /// @notice A factory for creating Cabal contracts.
 contract CabalFactory {
     /// @notice The address of the RelayerRegistry contract.
